b8c45051ac416297813b11ddfcdcc2f9a22f64c4
Fenntasy <vincent.billey@gmail.com>
2020-10-29 09:20:28 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/983e14f00e5fc19453e7739b97e837d138a38aa9 - 2020-10-29T09:20:28UTC

--- 02_programming_fundamentals/06_your_first_program/02_number_game/tests/numberGame.test.js
+++ 02_programming_fundamentals/06_your_first_program/02_number_game/tests/numberGame.test.js
@@ -16,7 +16,7 @@ describe("Number Game", () => {
   let fakeLog;
   beforeEach(() => {
     fakeLog = jest.spyOn(console, "log").mockImplementation(() => {});
-    Math.random = jest.fn().mockReturnValue(0.01);
+    Math.random = jest.fn().mockReturnValue(0.41);
   });
 
   afterEach(() => {
@@ -24,44 +24,61 @@ describe("Number Game", () => {
     fakeLog.mockRestore();
   });
 
-  it("Should handle 'too low' numbers", () => {
-    expect.assertions(1);
-    const reader = readerMockFactory([
-      (_question, callback) => callback(1),
-      (question, _callback) => {
-        expect(question.toLowerCase()).toMatch("too low");
-      },
-    ]);
-
+  it("Should handle the minimum (1) as a number to find", () => {
+    Math.random = jest.fn().mockReturnValue(0.0);
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(1)]);
     numberGame(reader);
+    expect(logs).toMatch(/you won/i);
+    fakeLog.mockRestore();
+  });
+
+  it("Should handle the maximum (100) as a number to find", () => {
+    Math.random = jest.fn().mockReturnValue(1);
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(100)]);
+    numberGame(reader);
+    expect(logs).toMatch(/you won/i);
+    fakeLog.mockRestore();
+  });
+
+  it("Should handle 'too low' numbers", () => {
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(), (_question, callback) => callback(2)]);
+    numberGame(reader);
+    expect(logs).toMatch(/too low/i);
+    fakeLog.mockRestore();
   });
 
   it("Should handle 'too high' numbers", () => {
-    expect.assertions(1);
-    const reader = readerMockFactory([
-      (_question, callback) => callback(3),
-      (question, _callback) => {
-        expect(question.toLowerCase()).toMatch("too high");
-      },
-    ]);
-
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(99)]);
     numberGame(reader);
+    expect(logs).toMatch(/too high/i);
+    fakeLog.mockRestore();
   });
 
   it("Should handle 'out of range' numbers", () => {
-    expect.assertions(2);
-    const reader = readerMockFactory([
-      (_question, callback) => callback(0),
-      (question, callback) => {
-        expect(question.toLowerCase()).toMatch("number is between");
-        callback(4);
-      },
-      (question, _callback) => {
-        expect(question.toLowerCase()).toMatch("number is between");
-      },
-    ]);
-
-    numberGame(reader, 1, 3);
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(123)]);
+    numberGame(reader);
+    expect(logs).toMatch(/number is between/i);
+    fakeLog.mockRestore();
   });
 
   it("Should handle the good number", () => {
@@ -69,8 +86,19 @@ describe("Number Game", () => {
     fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
       logs += message;
     });
-    const reader = readerMockFactory([(_question, callback) => callback(2)]);
+    const reader = readerMockFactory([(_question, callback) => callback(42)]);
     numberGame(reader);
+    expect(logs).toMatch(/won/i);
+    fakeLog.mockRestore();
+  });
+
+  it("Should be able to change min and max", () => {
+    let logs = "";
+    fakeLog = jest.spyOn(console, "log").mockImplementation((message) => {
+      logs += message;
+    });
+    const reader = readerMockFactory([(_question, callback) => callback(42)]);
+    numberGame(reader, 42, 42);
     expect(logs).toMatch(/won/i);
     fakeLog.mockRestore();
   });
