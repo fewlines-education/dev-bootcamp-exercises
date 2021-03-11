07773d07729e8906c6cb43314601626e10b7e325
Fenntasy <vincent.billey@gmail.com>
2021-03-11 11:25:36 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/c83c4ceffce5efdc1d91ce92d618fa3915fa7b80 - 2021-03-11T11:25:35UTC

--- 02_programming/01_algorithms_day_1/01_play_with_variables/src/01_play_with_variables.js
+++ 02_programming/01_algorithms_day_1/01_play_with_variables/src/01_play_with_variables.js
@@ -1,4 +1,4 @@
-// a constant called `question`, bound to a string: "Sparta, what is your profession?!";
+// a constant called `question`, bound to a string: "Spartan, what is your profession?!";
 
 // a constant called `answer`, bound to a string: "Dev dev dev!";
 
