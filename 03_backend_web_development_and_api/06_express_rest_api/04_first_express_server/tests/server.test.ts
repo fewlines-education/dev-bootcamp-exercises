e5ebfd9293277d205f2b4067cab9ca0db43c3a95
Fenntasy <vincent.billey@gmail.com>
2021-04-11 19:35:35 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/79f72b2fef46ff47d580f9ba1d75950b4b5467b5 - 2021-04-11T19:35:35UTC

--- 03_backend_web_development_and_api/06_express_rest_api/04_first_express_server/tests/server.test.ts
+++ 03_backend_web_development_and_api/06_express_rest_api/04_first_express_server/tests/server.test.ts
@@ -14,12 +14,16 @@ afterEach((done) => {
 
 describe("/ endpoint", () => {
   it("Should respond with a 200 HTTP code", () => {
+    expect.assertions(1);
+
     return fetch("http://localhost:3030/").then((response) => {
       expect(response.status).toBe(200);
     });
   });
 
   it("Should respond with OK", () => {
+    expect.assertions(1);
+
     return fetch("http://localhost:3030/")
       .then((response) => response.text())
       .then((result) => {
