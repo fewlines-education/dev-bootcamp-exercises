6fc1ff1818728ef0bc8adc8968994cb97eda2843
Fenntasy <vincent.billey@gmail.com>
2021-04-14 06:50:40 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/fec1607bc28e70c6e8ab774908e1833933e5d5a0 - 2021-04-14T06:50:40UTC

---
+++ 03_backend_web_development_and_api/08_api_create_update/01_warmup_subtotal/src/subtotal.ts
@@ -0,0 +1,12 @@
+type CartLine = {
+  product: string;
+  price: number;
+  quantity: number;
+};
+
+type CartLineWithSubtotal = {
+  product: string;
+  price: number;
+  quantity: number;
+  subtotal: number;
+};
