5d8b169c34e616466d6c2c5eaa0a5278f0db7dcf
Fenntasy <vincent.billey@gmail.com>
2021-08-20 14:28:01 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/77bda514aafe061d13756ac08cdc50d98915613d - 2021-08-20T14:28:00UTC

---
+++ 02_programming/05_this/01_fundamental_flowchart/src/shouldIMakeATicket.js
@@ -0,0 +1,8 @@
+function shouldIMakeATicket(isStuck, timeBeingStuck, hourOfTheDay){
+
+  // Code your function here
+
+}
+
+// Don't touch this for tests to execute properly
+module.exports = shouldIMakeATicket;
