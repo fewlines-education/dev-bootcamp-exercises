134daac89c657bc94eb70b0a30ef93b4c9f0699e
Fenntasy <vincent.billey@gmail.com>
2022-01-24 13:54:34 +0000

    Update from https://github.com/fewlinesco/sparta_courses/commit/57ce740e5ce17d7a002daa7ef6dc9330af086597 - 2022-01-24T13:54:33UTC

--- 02_programming/06_loops/01_greeter_for/tests/index.test.js
+++ 02_programming/06_loops/01_greeter_for/tests/index.test.js
@@ -1,8 +1,8 @@
 const helpers = require("camp2-helpers");
-const greeter = require("../src/index")
+const greeter = require("../src/index");
 
 let studentCode;
-beforeAll(async() => {
+beforeAll(async () => {
   // Loads the student's code
   studentCode = await helpers.readCode("./src/index.js");
   return studentCode;
@@ -11,11 +11,11 @@ beforeAll(async() => {
 test("print 'Hello <name>!' For every person", () => {
   expect.assertions(4);
 
-  jest.spyOn(console, "log")
+  jest.spyOn(console, "log");
   console.log.mockImplementation(() => {});
   greeter(["Joe", "Mike", "Robert"]);
 
-  expect(console.log).toHaveBeenCalledTimes(3)
+  expect(console.log).toHaveBeenCalledTimes(3);
   expect(console.log).toHaveBeenCalledWith("Hello Joe!");
   expect(console.log).toHaveBeenCalledWith("Hello Mike!");
   expect(console.log).toHaveBeenCalledWith("Hello Robert!");
@@ -26,7 +26,7 @@ test("print 'Hello <name>!' For every person", () => {
 test("A `for` loop should have been used", () => {
   expect.assertions(1);
 
-  const forRegexp = /for\s*\(.*?\)\s{/
+  const forRegexp = /for\s*\(.*?\)\s*{/;
 
   expect(forRegexp.test(studentCode)).toBe(true);
 });
